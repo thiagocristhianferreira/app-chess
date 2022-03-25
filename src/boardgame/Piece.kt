97cd4a91f6e2921a70227423ae21da8ab9758487
thiagocristhianferreira <thiagocristhian@gmail.com>
2022-03-25 16:32:03 -0300

    Satarting to implement Board and Piece

---
+++ src/boardgame/Piece.kt
@@ -0,0 +1,15 @@
+package boardgame
+
+class Piece() {
+
+    protected Position position;
+    private Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
+}
